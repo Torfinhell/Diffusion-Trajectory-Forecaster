4e02ae4d8b950c7f098d6207f03ff16dd4ad4878
Torfinhell <nikita2school@icloud.com>
2026-03-05 17:40:16 +0300

    dataloder finished

--- src/data_module/dataset.py
+++ src/data_module/dataset.py
@@ -10,7 +10,6 @@ from waymax import config, dataloader
 
 
 class DiffusionTrackerDataset(Dataset):
-    NUM_ZEROS = 5
     GLOBAL_ITER = None
 
     def __init__(
@@ -19,7 +18,6 @@ class DiffusionTrackerDataset(Dataset):
         gs_path: str,
         num_states: Optional[int] = None,
         download_path: Optional[str] = None,
-        start_ind: int = 0,
         max_num_objects: Optional[int] = None,
     ):
         super().__init__()
@@ -27,7 +25,7 @@ class DiffusionTrackerDataset(Dataset):
             waymax_config = getattr(config, waymax_conf_version)
             waymax_config = dataclasses.replace(
                 waymax_config,
-                path=f"{gs_path}{str(start_ind).zfill(self.__class__.NUM_ZEROS)}",
+                path=f"{gs_path}00000",
                 max_num_objects=max_num_objects,
             )
             self.__class__.GLOBAL_ITER = dataloader.simulator_state_generator(
@@ -48,13 +46,10 @@ class DiffusionTrackerDataset(Dataset):
                     state = next(self.__class__.GLOBAL_ITER)
                     self.states.append(state)
                 except Exception as e:
-                    inner_gs_path = (
-                        f"{gs_path}{str(ind).zfill(self.__class__.NUM_ZEROS)}"
-                    )
                     print(
                         f"Iteration through states finished with exception {e} \n"
-                        f"at ind: {ind} with start ind: {start_ind}, and max number of states: {num_states} \n"
-                        f"gs_path is {inner_gs_path}"
+                        f"at ind: {ind}, and max number of states: {num_states} \n"
+                        f"gs_path is {gs_path}00000"
                     )
                     break
             if download_path is not None:
