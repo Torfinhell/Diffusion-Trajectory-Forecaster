d575e339c2ace6b19119f36253e51d7d018fbc27
Torfinhell <nikita2school@icloud.com>
2026-03-18 16:33:15 +0300

    main model code done. Need pos_emb maybe also

--- src/models/diff_attn.py
+++ src/models/diff_attn.py
@@ -13,6 +13,7 @@ from .base_model import BaseDiffusionModel
 
 
 class PositionalEncoding(eqx.Module):  # TODO
+
     fc1: eqx.nn.Linear
     fc2: eqx.nn.Linear
     fc_out: eqx.nn.Linear
@@ -45,6 +46,7 @@ class SceneEncoder(eqx.Module):
     rnn_full_dim=num_heads*rnn_dim
     """
 
+    # agent_pos_emb: PositionalEncoding #TODO
     rnn_time: eqx.nn.MultiheadAttention | eqx.nn.LSTMCell
     sa_agents: eqx.nn.MultiheadAttention
     mlp: eqx.nn.MLP
@@ -62,9 +64,10 @@ class SceneEncoder(eqx.Module):
         num_agents: int,
         time_len: int,
         num_feat: int,
+        out_dim: int,
         key,
     ):
-        k1, k2, k3, self.dropout_key = jr.split(key, 4)
+        rnn_key, sa_key, mlp_key, self.dropout_key = jr.split(key, 4)
         self.rnn_type = rnn_type
         in_dim = num_agents * num_feat
         rnn_dim = in_dim
@@ -72,10 +75,13 @@ class SceneEncoder(eqx.Module):
             rnn_dim % rnn_num_heads == 0 or rnn_type == "lstm"
         ), "input rnn_dim should be divisable by rnn_num_heads"
         self.rnn_time = (
-            eqx.nn.LSTMCell(input_size=in_dim, hidden_size=rnn_dim, key=k1)
+            eqx.nn.LSTMCell(input_size=in_dim, hidden_size=rnn_dim, key=rnn_key)
             if rnn_type == "lstm"
             else eqx.nn.MultiheadAttention(
-                num_heads=rnn_num_heads, query_size=rnn_dim, dropout_p=drop_attn, key=k1
+                num_heads=rnn_num_heads,
+                query_size=rnn_dim,
+                dropout_p=drop_attn,
+                key=rnn_key,
             )
         )
         sa_dim = time_len * num_feat
@@ -83,16 +89,15 @@ class SceneEncoder(eqx.Module):
             sa_dim % sa_num_heads == 0
         ), "input sa_dim should be divisable by num_heads"
         self.sa_agents = eqx.nn.MultiheadAttention(
-            num_heads=sa_num_heads, query_size=sa_dim, dropout_p=drop_attn, key=k2
+            num_heads=sa_num_heads, query_size=sa_dim, dropout_p=drop_attn, key=sa_key
         )
         mlp_in_dim = time_len * num_feat
-        mlp_out_dim = time_len * num_feat
         self.mlp = eqx.nn.MLP(
             in_size=mlp_in_dim,
             width_size=mlp_dim,
             depth=max(num_mlp_layers - 1, 0),
-            out_size=mlp_out_dim,
-            key=k3,
+            out_size=out_dim,
+            key=mlp_key,
         )
 
     def __call__(self, x):  # (1, A, T, F)
@@ -115,123 +120,150 @@ class SceneEncoder(eqx.Module):
             x = self.rnn_time(x, x, x, key=self.dropout_key)  # mhsa
         x = rearrange(x, "t (a f) -> a (t f)", a=a)
         x = self.sa_agents(x, x, x, key=self.dropout_key)
-        return jax.vmap(lambda ag: self.mlp(ag))(x).reshape(1, a, t, f)  # (1, A, T, F)
+        return jax.vmap(lambda ag: self.mlp(ag))(x).reshape(a, -1)  # (1, A, out_dim)
 
 
-class SaMLP(eqx.Module):
-    scene_encoder: SceneEncoder
-    agent_pos_emb: PositionalEncoding
-    time_pos_emb: PositionalEncoding
-    out_shape: tuple[int, ...]
-    sa_mlp: None  # specify here array of self_attention+mlp(multiple blocks)
-
-    def __init__(
-        self, hid_dim: int, input_shape: list[int], output_shape: list[int], key
-    ):
-        k1, k2, k3 = jr.split(key, 3)
-        traj_dim, cond_dim = prod(output_shape), prod(input_shape)
-        self.out_shape = output_shape
-        self.fc1 = eqx.nn.Linear(traj_dim + cond_dim + 1, hid_dim, key=k1)
-        self.fc2 = eqx.nn.Linear(hid_dim, hid_dim, key=k2)
-        self.fc_out = eqx.nn.Linear(hid_dim, traj_dim, key=k3)
-
-    def __call__(self, t, x_t, cond):
-        x = jnp.concatenate(
-            [x_t.reshape(-1), cond.reshape(-1), jnp.atleast_1d(t)], axis=0
-        )
-        x = jnn.relu(self.fc1(x))
-        x = jnn.relu(self.fc2(x))
-        return self.fc_out(x).reshape(self.out_shape)
-
-
-class SaCaMLP2(eqx.Module):
-    scene_encoder: SceneEncoder
-    agent_pos_emb: PositionalEncoding
-    time_pos_emb: PositionalEncoding
-    out_shape: tuple[int, ...]
-    sa_mlp: None  # specify here array of self_attention+mlp(multiple blocks)
+class AttentionMLP(eqx.Module):
+    attn: eqx.nn.MultiheadAttention
+    dropout_key: jax.random.PRNGKey
+    type_attn: Literal["cross", "self"]
+    mlp: eqx.nn.MLP
 
     def __init__(
-        self, hid_dim: int, input_shape: list[int], output_shape: list[int], key
+        self,
+        attn_dim: int,
+        attn_num_heads: int,
+        out_dim: int,
+        mlp_dim: int,
+        num_mlp_layers: int,
+        drop_attn: float,
+        type_attn: Literal["cross", "self"],
+        key,
+        kv_dim=None,
     ):
-        k1, k2, k3 = jr.split(key, 3)
-        traj_dim, cond_dim = prod(output_shape), prod(input_shape)
-        self.out_shape = output_shape
-        self.fc1 = eqx.nn.Linear(traj_dim + cond_dim + 1, hid_dim, key=k1)
-        self.fc2 = eqx.nn.Linear(hid_dim, hid_dim, key=k2)
-        self.fc_out = eqx.nn.Linear(hid_dim, traj_dim, key=k3)
-
-    def __call__(self, t, x_t, cond):
-        x = jnp.concatenate(
-            [x_t.reshape(-1), cond.reshape(-1), jnp.atleast_1d(t)], axis=0
+        attn_key, mlp_key, self.dropout_key = jr.split(key, 3)
+        assert (
+            attn_dim % attn_num_heads == 0
+        ), "input attn_dim should be diviattnble by num_heads"
+        self.type_attn = type_attn
+        if self.type_attn == "self":
+            self.attn = eqx.nn.MultiheadAttention(
+                num_heads=attn_num_heads,
+                query_size=attn_dim,
+                dropout_p=drop_attn,
+                key=attn_key,
+            )
+        elif self.type_attn == "cross":
+            self.attn = eqx.nn.MultiheadAttention(
+                num_heads=attn_num_heads,
+                query_size=attn_dim,
+                key_size=kv_dim,
+                value_size=kv_dim,
+                dropout_p=drop_attn,
+                key=attn_key,
+            )
+        mlp_in_dim = attn_dim
+        self.mlp = eqx.nn.MLP(
+            in_size=mlp_in_dim,
+            width_size=mlp_dim,
+            depth=max(num_mlp_layers - 1, 0),
+            out_size=out_dim,
+            key=mlp_key,
         )
-        x = jnn.relu(self.fc1(x))
-        x = jnn.relu(self.fc2(x))
-        return self.fc_out(x).reshape(self.out_shape)
+
+    def __call__(self, x, kv_cond=None):  # x: (1, a, t_future*f)
+        a, _ = x.shape
+        if self.type_attn == "self":
+            x = self.attn(x, x, x, key=self.dropout_key)
+        elif self.type_attn == "cross":
+            x = self.attn(
+                x, kv_cond, kv_cond, mask=jnp.diag(jnp.ones(a)), key=self.dropout_key
+            )
+        return jax.vmap(self.mlp)(x)
 
 
 class DiffAttention(eqx.Module):
     scene_encoder: SceneEncoder
     out_shape: tuple[int, ...]
-    # sa_ca_mlp_2: list[SaCaMLP2]
-    # sa_mlp: list[SaMLP]
+    ca_mlp_layers: list[AttentionMLP]
+    sa_mlp_layers: list[AttentionMLP]
+    num_sa_mlp: int
 
     def __init__(
         self,
         se_args,
-        sacamlp_args,
-        num_saca_mlp,
-        sacamlp2_args,
-        num_sacamlp2,
+        samlp_args,
+        num_sa_mlp,
+        camlp_args,
+        num_camlp,
         out_shape: list[int],
+        final_out_dim: int,
         key,
     ):
-        se_key, sa_ca_mlp_2_key, sacamlp_key = jr.split(key, 3)
+        se_key, ca_mlp_key, sacamlp_key = jr.split(key, 3)
         self.scene_encoder = SceneEncoder(**se_args, key=se_key)
-        # sacamlp_keys = jr.split(sacamlp_key, num_saca_mlp)
-        # self.sa_mlp=[SaMLP(**sacamlp_args, key=sacamlp_key) for _, sacamlp_key in zip(range(num_saca_mlp), sacamlp_keys)]
-        # sa_ca_mlp_2_keys = jr.split(sa_ca_mlp_2_key, num_sacamlp2)
-        # self.sa_ca_mlp_2=[SaCaMLP2(**sacamlp2_args, key=sa_ca_mlp_2_key) for _, sa_ca_mlp_2_key in zip(range(num_sacamlp2),sa_ca_mlp_2_keys)]
+        self.num_sa_mlp = num_sa_mlp
+        sacamlp_keys = jr.split(sacamlp_key, num_sa_mlp)
+        self.sa_mlp_layers = [
+            AttentionMLP(**samlp_args, key=sacamlp_key, type_attn="self")
+            for _, sacamlp_key in zip(range(num_sa_mlp), sacamlp_keys)
+        ]
+        ca_mlp_keys = jr.split(ca_mlp_key, num_camlp)
+        self.ca_mlp_layers = [
+            AttentionMLP(**camlp_args, key=ca_mlp_key, type_attn="cross")
+            for _, ca_mlp_key in zip(range(num_camlp - 1), ca_mlp_keys[:-1])
+        ]
+        if num_camlp > 1:
+            camlp_args["out_dim"] = final_out_dim
+            self.ca_mlp_layers.append(
+                AttentionMLP(**camlp_args, key=ca_mlp_key, type_attn="cross")
+            )
         self.out_shape = out_shape
 
-    def __call__(self, t, x_t, cond):
-        KV_cond = self.scene_encoder(cond)
-        x = jnp.concat([t, x_t], axis=-1)
-        # for layer in self.sa_mlp:
-        #     x=layer(x) #TODO apply lax scan
-        # for layer in self.sa_ca_mlp_2:
-        #     x=layer(x) #TODO apply lax scan
-        # x=self.sa_ca_mlp_2(x, KV_cond) #apply lax scan
-        return x.reshape(self.out_shape)
+    def __call__(self, t_noise, x_t, cond):
+        KV_cond = self.scene_encoder(cond)  # (1, A, out_dim)
+        _, a, t, f = x_t.shape
+        x_t = jnp.concat(
+            [repeat(t_noise, " -> 1 a 1 f", a=a, f=f), x_t], axis=-2
+        ).reshape(a, -1)
+        # x_t, _=jax.lax.scan(lambda input, layer_idx:(self.sa_mlp_layers[layer_idx](input), None), x_t.reshape(a, -1), jnp.arange(self.num_sa_mlp)) #TODO
+        for layer in self.sa_mlp_layers:
+            x_t = layer(x_t)
+        for layer in self.ca_mlp_layers:
+            x_t = layer(x_t, KV_cond)
+        return x_t.reshape(self.out_shape)
 
 
 class DiffusionAttentionModel(BaseDiffusionModel):
     def __init__(
         self,
         se_args,
-        sacamlp_args,
-        num_saca_mlp,
-        sacamlp2_args,
-        num_sacamlp2,
+        samlp_args,
+        num_sa_mlp,
+        camlp_args,
+        num_camlp,
+        final_out_dim,
         output_shape: list[int],
         **kwargs
     ):
         self.se_args = se_args
-        self.sacamlp_args = sacamlp_args
-        self.num_saca_mlp = num_saca_mlp
-        self.sacamlp2_args = sacamlp2_args
-        self.num_sacamlp2 = num_sacamlp2
+        self.samlp_args = samlp_args
+        self.num_sa_mlp = num_sa_mlp
+        self.camlp_args = camlp_args
+        self.num_camlp = num_camlp
         self.output_shape = output_shape
+        self.final_out_dim = final_out_dim
         super().__init__(**kwargs)
 
     def get_model(self, key_model):
         return DiffAttention(
             se_args=self.se_args,
-            sacamlp_args=self.sacamlp_args,
-            num_saca_mlp=self.num_saca_mlp,
-            sacamlp2_args=self.sacamlp2_args,
-            num_sacamlp2=self.num_sacamlp2,
+            samlp_args=self.samlp_args,
+            num_sa_mlp=self.num_sa_mlp,
+            camlp_args=self.camlp_args,
+            num_camlp=self.num_camlp,
             out_shape=self.output_shape,
+            final_out_dim=self.final_out_dim,
             key=key_model,
         )
 
