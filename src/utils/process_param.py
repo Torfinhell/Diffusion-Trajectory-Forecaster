2f14ce33a4be25dfdfa5d9879feb0aa2d1b25213
Torfinhell <nikita2school@icloud.com>
2026-05-10 16:54:52 +0300

    minor fix

--- src/utils/process_param.py
+++ src/utils/process_param.py
@@ -1,4 +1,5 @@
 import hashlib
+import math
 import os
 import socket
 import subprocess
@@ -10,7 +11,7 @@ import yaml
 from hydra.core.hydra_config import HydraConfig
 from hydra.utils import to_absolute_path
 from omegaconf import OmegaConf
-import math
+
 
 def resolve_scheduler_decay_steps(hparams, dm) -> None:
     scheduler_cfg = hparams.get("scheduler")
@@ -19,12 +20,13 @@ def resolve_scheduler_decay_steps(hparams, dm) -> None:
 
     train_steps = len(dm.train_dataloader())
     limit_train_batches = hparams.trainer.train_epoch_len
-    if isinstance(limit_train_batches, float):
+    if limit_train_batches is not None:
         train_steps = max(1, math.ceil(train_steps * limit_train_batches))
     else:
         train_steps = min(train_steps, int(limit_train_batches))
 
     scheduler_cfg.decay_steps = int(train_steps * hparams.trainer.num_epochs)
+
 
 def process_hparams(hparams, print_hparams=True):
     OmegaConf.set_struct(hparams, False)
