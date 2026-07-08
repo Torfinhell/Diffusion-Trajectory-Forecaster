c6af3b5684f57e9eb25518eef7b7e476e1d7bb3b
Andrey Rudakov <100122750+Andrey793@users.noreply.github.com>
2026-07-08 19:03:33 +0300

    removed useless model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,4 @@
 from src.models.diff_attention import DiffAttention
 from src.models.diff_linear import DiffLinear
-from src.models.diff_vbd import DiffVBD
 
-__all__ = ["DiffAttention", "DiffLinear", "DiffVBD"]
+__all__ = ["DiffAttention", "DiffLinear"]
